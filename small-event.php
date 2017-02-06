e99b963b73e3a60e4b98803c1bce94c7549b69f7
Floris Jansen <florismartijnjansen@gmail.com>
2017-02-06 17:43:22 +0100

    give event thumbnail the same height

--- small-event.php
+++ small-event.php
@@ -1,12 +1,14 @@
 <article class="event--small">
 	<a href="<?php the_permalink(); ?>"
 		class="event--small__anchor">
-		<?php
-			the_post_thumbnail(
-				'medium',
-				array('class' => 'event--small__thumb')
-			);
-		?>
+		<div class="event--small__thumb">
+			<?php
+				the_post_thumbnail(
+					'medium',
+					array('class' => 'event--small__img')
+				);
+			?>
+		</div>
 		<h3 class="event--small__name"><?php the_title(); ?></h3>
 		<p class="event--small__descr"><?php echo get_the_excerpt(); ?></p>
 	</a>

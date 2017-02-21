586fc47fa130fd8002b70ee26cb4742536edea2d
Floris Jansen <florismartijnjansen@gmail.com>
2017-02-21 13:49:24 +0100

    add elements for layout to event archive (#3)

--- archive-event.php
+++ archive-event.php
@@ -1,9 +1,24 @@
 <?php get_header(); ?>
 
-<?php while(have_posts()) : the_post(); ?>
+<main class="events--small">
 
-	<?php include( 'small-event.php' ); ?>
+	<h1><?php echo esc_attr_x( 'Events', 'archive title' ); ?></h1>
 
-<?php endwhile; ?>
+	<?php while(have_posts()) : the_post(); ?>
+
+		<?php include( 'inc/small-event.php' ); ?>
+
+	<?php endwhile; ?>
+
+	<nav class="pagination"><?php
+		$big = 999999999;
+		echo paginate_links( array(
+			'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+			'format' => '?paged=%#%',
+			'current' => max( 1, get_query_var('paged') ),
+			'total' => $wp_query->max_num_pages
+		) ); ?></nav>
+
+</main>
 
 <?php get_footer(); ?>

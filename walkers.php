edfc3b85870fac5a7d56e26b1360309da08a2c61
Floris Jansen <florismartijnjansen@gmail.com>
2017-02-21 13:14:15 +0100

    add walker for sitemap (#7)

---
+++ walkers.php
@@ -0,0 +1,74 @@
+<?php
+
+class Walker_Sitemap extends Walker_Nav_Menu
+{
+	/**
+	 * Start the element output.
+	 *
+	 * @param	string $output Passed by reference. Used to append additional content.
+	 * @param	object $item	 Menu item data object.
+	 * @param	int $depth		 Depth of menu item. May be used for padding.
+	 * @param	array $args		Additional strings.
+	 * @return void
+	 */
+	public function start_el( &$output, $item, $depth, $args )
+	{
+		$output		 .= '<li class="sitemap__item">';
+		$attributes	= '';
+
+		! empty ( $item->url )
+			and $attributes .= ' href="' . esc_attr( $item->url ) .'"';
+
+		$attributes .= ' class="sitemap__link"';
+
+		$attributes	= trim( $attributes );
+		$title			 = apply_filters( 'the_title', $item->title, $item->ID );
+		$item_output = $args->before;
+		$item_output .= "<a $attributes>";
+		$item_output .= $args->link_before . $title;
+		$item_output .= "</a>";
+		$item_output .= $args->link_after . $args->after;
+
+		// Since $output is called by reference we don't need to return anything.
+		$output .= apply_filters(
+			'walker_nav_menu_start_el',
+			$item_output,
+			$item,
+			$depth,
+			$args
+		);
+	}
+
+	/**
+	 * @see Walker::start_lvl()
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @return void
+	 */
+	public function start_lvl( &$output )
+	{
+		$output .= '<ul class="sitemap__sub-menu">';
+	}
+
+	/**
+	 * @see Walker::end_lvl()
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @return void
+	 */
+	public function end_lvl( &$output )
+	{
+		$output .= '</ul>';
+	}
+
+	/**
+	 * @see Walker::end_el()
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @return void
+	 */
+	function end_el( &$output )
+	{
+		$output .= '</li>';
+	}
+}

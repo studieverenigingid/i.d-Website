9d55d43aee1971e105f4edbc4589b011bb4d9a57
Floris Jansen <florismartijnjansen@gmail.com>
2017-02-21 10:23:20 +0100

    add styling to top elements in page

--- single-event.php
+++ single-event.php
@@ -4,51 +4,51 @@
 
 
 
-	<div class="event--page__thumb">
-		<?php if ( has_post_thumbnail() ) :
-			the_post_thumbnail(
-				'large',
-				array('class' => 'event--page__img')
-			);
-		endif; ?>
-	</div>
+	<header class="event--page__header">
 
+		<div class="event--page__short-info">
 
-
-	<header class="event--page__date-and-name">
-
-		<?php
-			$start = new DateTime(get_field('start_datetime'));
-			$start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
-
-			$end = new DateTime(get_field('end_datetime'));
-			$end->setTimezone( new DateTimeZone('Europe/Amsterdam') );
-
-			$month = $start->format('M');
-			$day   = $start->format('j');
-		?>
-		<div class="date-block">
 			<?php
-				echo '<div class="date-block__month">' . $month . '</div>';
-				echo '<div class="date-block__day">' . $day . '</div>';
+				$start = new DateTime(get_field('start_datetime'));
+				$start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+
+				$end = new DateTime(get_field('end_datetime'));
+				$end->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+
+				$month = $start->format('M');
+				$day   = $start->format('j');
 			?>
+			<div class="date-block">
+				<?php
+					echo '<div class="date-block__month">' . $month . '</div>';
+					echo '<div class="date-block__day">' . $day . '</div>';
+				?>
+			</div>
+
+			<h1 class="event--page__name"><?php the_title(); ?></h1>
+
+			<?php
+				$start_time = $start->format('H:i');
+				$end_time   = $end->format('H:i');
+			?>
+			<div class="event--page__time time-block">
+				<?php
+					echo $start_time . " – " . $end_time;
+				?>
+			</div>
+
+			<div class="event--page__thumb">
+				<?php if ( has_post_thumbnail() ) :
+					the_post_thumbnail(
+						'large',
+						array('class' => 'event--page__img')
+					);
+				endif; ?>
+			</div>
+
 		</div>
 
-		<h1 class="event--page__name"><?php the_title(); ?></h1>
-
 	</header>
-
-
-
-	<?php
-		$start_time = $start->format('H:i');
-		$end_time   = $end->format('H:i');
-	?>
-	<div class="time-block">
-		<?php
-			echo $start_time . " – " . $end_time;
-		?>
-	</div>
 
 
 

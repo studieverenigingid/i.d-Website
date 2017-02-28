de7966ad4cc1cd4410b97234474cfea667080731
Floris Jansen <florismartijnjansen@gmail.com>
2017-02-28 13:06:54 +0100

    put grid around footer elements

--- footer.php
+++ footer.php
@@ -1,23 +1,27 @@
 		<?php global $img_folder; ?>
 		<footer class="pri-footer">
 
-			<a href="<?php echo get_site_url(); ?>">
-				<picture>
-					<source srcset="<?=$img_folder?>logo.svg" type="image/svg+xml">
-					<img class="pri-footer__logo" alt="Study association i.d"
-						srcset="<?=$img_folder?>logo.png 1x,
-							<?=$img_folder?>logo@2x.png 2x"
-						src="<?=$img_folder?>logo.png">
-				</picture>
-			</a>
+			<div class="pri-footer__association pri-footer__col">
+				<a href="<?php echo get_site_url(); ?>">
+					<picture>
+						<source srcset="<?=$img_folder?>logo.svg" type="image/svg+xml">
+						<img class="pri-footer__logo" alt="Study association i.d"
+							srcset="<?=$img_folder?>logo.png 1x,
+								<?=$img_folder?>logo@2x.png 2x"
+							src="<?=$img_folder?>logo.png">
+					</picture>
+				</a>
+			</div>
 
-			<h2 class="sitemap__heading">Sitemap</h2>
-			<?php wp_nav_menu( array(
-				'theme_location' => 'sitemap',
-				'container' => 'nav',
-				'container_class' => 'sitemap',
-				'menu_class' => 'sitemap__list',
-				'walker' => new Walker_Sitemap() ) ); ?>
+			<div class="pri-footer__sitemap pri-footer__col">
+				<h2 class="sitemap__heading">Sitemap</h2>
+				<?php wp_nav_menu( array(
+					'theme_location' => 'sitemap',
+					'container' => 'nav',
+					'container_class' => 'sitemap',
+					'menu_class' => 'sitemap__list',
+					'walker' => new Walker_Sitemap() ) ); ?>
+			</div>
 
 		</footer>
 

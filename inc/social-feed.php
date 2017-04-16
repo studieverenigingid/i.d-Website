7e0707c4fdb8b50d60de09948c7875bcd10667d5
nwesthoff <nilsepils94@hotmail.com>
2017-04-16 02:03:38 +0200

    v0.1 Instagram API connection

--- inc/social-feed.php
+++ inc/social-feed.php
@@ -1,4 +1,5 @@
 <?php 
+	// Vimeo API
 
 		// include autoload.php from the vimeo php library
 		require("/API/vimeo/autoload.php"); 
@@ -40,20 +41,57 @@
 				<div class='social__title social__title--vimeo'><i class='fa fa-vimeo'></i> Vimeo</div></a></div>";
 		}
 
-		// loop through each video from the user
-		foreach($videos['body']['data'] as $video) {
 
-			// get the link to the video
-			$link = $video['link'];
-			$title = $video['name'];
+	// Instagram API
+
+		// Connect to Instagram
+
+		function connectToInstagram($url){
+			$ch = curl_init();
+
+			curl_setopt_array($ch, array(
+				CURLOPT_URL => $url,
+			 	CURLOPT_RETURNTRANSFER => true,
+			 	CURLOPT_SSL_VERIFYPEER => false,
+			 	CURLOPT_SSL_VERIFYHOST => 2
+			));
+			$result = curl_exec($ch);
+			curl_close($ch);
+			return $result;
+		}
+
+		function latestPosts(){
+			$options = get_option('id_settings');
+			$instagram_access_token = $options['id_instagram_access_token_field'];
+			$url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token='.$instagram_access_token.'&?count=3';
+			$instagramInfo = connectToInstagram($url);
+			$results = json_decode($instagramInfo, true);
+
+			foreach ($results['data'] as $post) {
+				$image_url = $post['images']['standard_resolution']['url'];
+				$link = $post['link'];
+				
+				echo "<div class='social__container--insta' style='background-image:url(".$image_url.");' ><a class='social__link' href=".$link." target='_blank' >
+				<div class='social__title social__title--insta'><i class='fa fa-instagram'></i> Instagram</div></a></div>"; 
+			}
+		}
+
+		latestPosts();
+
+		/* loop through each picture from the user
+		foreach($pictures['body']['data'] as $picture) {
+
+			// get the link to the picture
+			$link = $picture['link'];
+			$title = $picture['name'];
 
 			// get the largest picture "thumb"
-			$pictures = $video['pictures']['sizes'];
+			$pictures = $picture['pictures']['sizes'];
 			$largestPicture = $pictures[count($pictures) - 2]['link'];
 
 			echo "<div class='social__container--insta' style='background-image:url(".$largestPicture.");' ><a class='social__link' href=".$link." target='_blank' >
-				<div class='social__title social__title--insta'><i class='fa fa-instagram'></i> Instagram</div></a></div>";
+				<div class='social__title social__title--insta'><i class='fa fa-instagram'></i> Instagram</div></a></div>"; 
 		}
-
+		*/
 
 	?>
--- inc/theme-settings.php
+++ inc/theme-settings.php
@@ -12,31 +12,64 @@
 
 	function id_settings_init(  ) { 
 
-		register_setting( 'themePage', 'id_settings' );
+		register_setting( 'vimeoSettings', 'id_settings' );
 
 		add_settings_section(
-			'id_themePage_section', 
+			'id_vimeoSettings_section', 
 			__( 'Vimeo API settings', 'wordpress' ), 
-			'id_settings_section_callback', 
-			'themePage'
+			'id_vimeoSettings_section_callback', 
+			'vimeoSettings'
 		);
 
 		add_settings_field( 
 			'id_vimeo_api_id_field', 
 			__( 'Vimeo API ID', 'wordpress' ), 
 			'id_vimeo_api_id_render', 
-			'themePage', 
-			'id_themePage_section' 
+			'vimeoSettings', 
+			'id_vimeoSettings_section' 
 		);
 
 		add_settings_field( 
 			'id_vimeo_api_secret_field', 
 			__( 'Vimeo API Secret', 'wordpress' ), 
 			'id_vimeo_api_secret_render', 
-			'themePage', 
-			'id_themePage_section' 
+			'vimeoSettings', 
+			'id_vimeoSettings_section' 
 		);
-		
+
+		register_setting( 'instagramSettings', 'id_settings' );
+
+		add_settings_section(
+			'id_instagramSettings_section', 
+			__( 'Instagram API settings', 'wordpress' ), 
+			'id_instagramSettings_section_callback', 
+			'instagramSettings'
+		);
+
+		add_settings_field( 
+			'id_instagram_client_id_field', 
+			__( 'Instagram client ID', 'wordpress' ), 
+			'id_instagram_client_id_render', 
+			'instagramSettings', 
+			'id_instagramSettings_section' 
+		);
+
+		add_settings_field( 
+			'id_instagram_client_secret_field', 
+			__( 'Instagram client secret', 'wordpress' ), 
+			'id_instagram_client_secret_render', 
+			'instagramSettings', 
+			'id_instagramSettings_section' 
+		);
+
+		add_settings_field( 
+			'id_instagram_access_token_field', 
+			__( 'Instagram access token', 'wordpress' ), 
+			'id_instagram_access_token_render', 
+			'instagramSettings', 
+			'id_instagramSettings_section' 
+		);
+
 	}
 
 
@@ -60,14 +93,62 @@
 	}
 
 
-	function id_settings_section_callback(  ) { 
+	function id_vimeoSettings_section_callback(  ) { 
 
 		echo __( 'Input Vimeo API keys', 'wordpress' );
 
 	}
 
+	function id_instagram_client_id_render(  ) { 
 
-	function id_options_page(  ) { 
+		$options = get_option( 'id_settings' );
+		?>
+		<input type='text' name='id_settings[id_instagram_client_id_field]' value='<?php echo $options['id_instagram_client_id_field']; ?>'>
+		<?php
+
+	}
+
+	function id_instagram_client_secret_render(  ) { 
+
+		$options = get_option( 'id_settings' );
+		?>
+		<input type='text' name='id_settings[id_instagram_client_secret_field]' value='<?php echo $options['id_instagram_client_secret_field']; ?>'>
+		<?php
+
+	}
+
+	function id_instagram_access_token_render(  ) { 
+
+		$options = get_option( 'id_settings' );
+
+		if (!empty($options['id_instagram_client_id_field']) && !empty($options['id_instagram_client_secret_field'])){
+			if (!empty($options['id_instagram_access_token_field'])){
+				?>
+					<input type='text' name='id_settings[id_instagram_access_token_field]' value='<?php echo $options['id_instagram_access_token_field']; ?>'>
+				<?php
+			}
+			elseif (!empty($_GET['code'])) {
+				?>
+					<input type='text' name='id_settings[id_instagram_access_token_field]' value='<?php get_instagram_access_token(); ?>'>
+				<?php
+			} else {
+				echo '<a class="button-primary" href="https://api.instagram.com/oauth/authorize/?client_id='.$options[id_instagram_client_id_field].'&redirect_uri='.admin_url( 'options-general.php?page=i.d-website_settings').'&response_type=code">Log in with Instagram</a>';
+			}
+		} else {
+			?> <p>Fill in your instagram client ID and Secret first</p> <?php
+		}
+
+	}
+
+	function id_instagramSettings_section_callback(  ) { 
+		$options = get_option( 'id_settings' );
+
+		echo __( 'Enter your Instagram Client ID, then log in to your Instagram account to get your access token', 'wordpress' );
+
+	}
+
+
+	function id_options_page() { 
 
 		?>
 		<form action='options.php' method='post'>
@@ -75,13 +156,40 @@
 			<h2>i.d-Website Settings</h2>
 
 			<?php
-			settings_fields( 'themePage' );
-			do_settings_sections( 'themePage' );
+			settings_fields( 'vimeoSettings' );
+			do_settings_sections( 'vimeoSettings' );
+
+			settings_fields( 'instagramSettings' );
+			do_settings_sections( 'instagramSettings' );
 			submit_button();
 			?>
 
 		</form>
 		<?php
 
+	}
+
+	function get_instagram_access_token(){
+		$options = get_option('id_settings');
+		$code = $_GET['code'];
+		$url = 'https://api.instagram.com/oauth/access_token';
+		$access_token_settings = array(	'client_id' => $options['id_instagram_client_id_field'],
+										'client_secret' => $options['id_instagram_client_secret_field'],
+										'grant_type' => 'authorization_code',
+										'redirect_uri' => admin_url( 'options-general.php?page=i.d-website_settings'),
+										'code' => $code
+									 );
+
+		$curl = curl_init( $url );
+		curl_setopt( $curl, CURLOPT_POST, true);
+		curl_setopt( $curl, CURLOPT_POSTFIELDS, $access_token_settings);
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false);
+
+		$result = curl_exec($curl);
+		curl_close($curl);
+
+		$results = json_decode($result, true);
+		echo $results['access_token'];
 	}
 ?>

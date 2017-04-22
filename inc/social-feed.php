bdd42fa89305aaaf40ee6b2dcff5a36417b8648e
Floris Jansen <florismartijnjansen@gmail.com>
2017-04-22 12:42:37 +0200

    gracefully fail without Vimeo API + make both social functions use same options call and function layout

--- inc/social-feed.php
+++ inc/social-feed.php
@@ -1,49 +1,65 @@
-<?php 
-	// Vimeo API
+<?php
 
-		// include autoload.php from the vimeo php library
-		require("/API/vimeo/autoload.php"); 
+		// Wordpress options
+		$options = get_option('id_settings');
 
-		$options = get_option( 'id_settings' );
+		// Vimeo API
 
-		// The client id and client secret needed to use the vimeo API
-		$client_id = $options['id_vimeo_api_id_field'];
-		$client_secret = $options['id_vimeo_api_secret_field'];
+		function connectToVimeo($options) {
 
-		$scope = "public";
+			// include autoload.php from the vimeo php library
+			$Vimeo_API_uri = "/API/vimeo/autoload.php";
+			if (file_exists($Vimeo_API_uri)): // Check if the API is present
+				require $Vimeo_API_uri; // If so, include it
+			else:
+				return false;
+			endif;
 
-		$userId = "studieverenigingid";
+			// The client id and client secret needed to use the vimeo API
+			$client_id = $options['id_vimeo_api_id_field'];
+			$client_secret = $options['id_vimeo_api_secret_field'];
 
-		// initialize the vimeo library
-		$lib = new \Vimeo\Vimeo($client_id, $client_secret);
+			$scope = "public";
 
-		// request an auth token (needed for all requests to the Vimeo API)
-		$token = $lib->clientCredentials($scope);
+			$userId = "studieverenigingid";
 
-		// set the token
-		$lib->setToken($token['body']['access_token']);
+			// initialize the vimeo library
+			$lib = new \Vimeo\Vimeo($client_id, $client_secret);
 
-		// request all of a user's videos, 50 per page
-		$videos = $lib->request("/users/$userId/videos", ['per_page' => 3]);
+			// request an auth token (needed for all requests to the Vimeo API)
+			$token = $lib->clientCredentials($scope);
 
-		// loop through each video from the user
-		foreach($videos['body']['data'] as $video) {
+			// set the token
+			$lib->setToken($token['body']['access_token']);
 
-			// get the link to the video
-			$link = $video['link'];
-			$title = $video['name'];
-			$date = date('M j, Y', strtotime($video['created_time']));
+			// request all of a user's videos, 50 per page
+			$videos = $lib->request("/users/$userId/videos", ['per_page' => 3]);
 
-			// get the largest picture "thumb"
-			$pictures = $video['pictures']['sizes'];
-			$largestPicture = $pictures[count($pictures) - 2]['link'];
+			return $videos;
+		}
 
-			echo "<a class='social__link' href=".$link." target='_blank' ><div class='social__container--vimeo' style='background-image:url(".$largestPicture.");' >
-				<div class='social__title social__title--vimeo'><i class='fa fa-vimeo'></i> Vimeo</div></div></a>";
+		function latestVimeoPosts() {
+			$videos = connectToVimeo();
+
+			// loop through each video from the user
+			foreach($videos['body']['data'] as $video) {
+
+				// get the link to the video
+				$link = $video['link'];
+				$title = $video['name'];
+				$date = date('M j, Y', strtotime($video['created_time']));
+
+				// get the largest picture "thumb"
+				$pictures = $video['pictures']['sizes'];
+				$largestPicture = $pictures[count($pictures) - 2]['link'];
+
+				echo "<a class='social__link' href=".$link." target='_blank' ><div class='social__container--vimeo' style='background-image:url(".$largestPicture.");' >
+					<div class='social__title social__title--vimeo'><i class='fa fa-vimeo'></i> Vimeo</div></div></a>";
+			}
 		}
 
 
-	// Instagram API
+		// Instagram API
 
 		// Connect to Instagram
 
@@ -61,8 +77,7 @@
 			return $result;
 		}
 
-		function latestPosts(){
-			$options = get_option('id_settings');
+		function latestInstaPosts($options) {
 			$instagram_access_token = $options['id_instagram_access_token_field'];
 			$url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token='.$instagram_access_token.'&count=3';
 			$instagramInfo = connectToInstagram($url);
@@ -72,12 +87,13 @@
 				$image_url = $post['images']['standard_resolution']['url'];
 				$link = $post['link'];
 				$date = date('M j, Y', $post['created_time']);
-				
+
 				echo "<a class='social__link' href=".$link." target='_blank' ><div class='social__container--insta' style='background-image:url(".$image_url.");' >
-				<div class='social__title social__title--insta'><i class='fa fa-instagram'></i> Instagram</div></div></a>	"; 
+				<div class='social__title social__title--insta'><i class='fa fa-instagram'></i> Instagram</div></div></a>";
 			}
 		}
 
-		latestPosts();
+		latestVimeoPosts($options);
+		latestInstaPosts($options);
 
 	?>

1337585eaa7703842b677cc48e2f53e7acf6d353
Floris Jansen <florismartijnjansen@gmail.com>
2017-04-25 17:20:12 +0200

    add single vacancy page :D

---
+++ single-vacancy.php
@@ -0,0 +1,96 @@
+<?php
+	get_header();
+?>
+
+<?php
+	if(have_posts()) : while(have_posts()) : the_post();
+?>
+
+	<header class="vacancy__header">
+
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail('post-thumbnail',
+				array( 'class' => 'vacancy__logo--large')
+			); ?>
+		<?php endif; ?>
+
+		<?php
+			$company = get_field('company');
+		?>
+		<p class="vacancy__intro  vacancy__intro--large">
+			<span class="vacancy__company"><?=$company?></span> is looking for a
+		</p>
+
+		<h1 class="vacancy__title  vacancy__title--large"><?php the_title(); ?></h1>
+
+		<p class="vacancy__details  vacancy__details--large">
+			<?php
+				$location = get_field('location');
+				if ( !empty($location) ) { ?>
+					<span class="vacancy__tag  vacancy__location">
+			    	<i class="fa fa-map-marker"></i> <?=$location?>
+					</span>
+			<?php	} ?>
+
+			&bull;
+
+			<?php
+				$categories = get_the_category();
+				if ( !empty($categories) ) {
+					$category = esc_html( $categories[0]->name ); ?>
+					<span class="vacancy__tag  vacancy__type">
+			    	<?=$category?>
+					</span>
+			<?php	} ?>
+		</p>
+
+	</header>
+
+	<main class="primary-content news--page__content">
+
+		<div class="vacancy__button-group">
+			<?php
+				$file = get_field('vacancy_attachment');
+				if ( $file ): ?>
+					<a target="_blank" class="button  button--light  vacancy__button"
+						href="<?php echo $file['url']; ?>">
+						<i class="fa fa-file-text-o"></i> Download this description
+					</a>
+			<?php endif; ?>
+
+			<?php
+				$apply = get_field('apply_directly');
+				if ( $apply ): ?>
+				<a target="_blank" class="button  vacancy__button"
+					href="<?php echo $apply ?>">
+					<i class="fa fa-send"></i> Apply directly
+				</a>
+			<?php endif; ?>
+		</div>
+
+		<?php the_content(); ?>
+
+		<div class="vacancy__button-group">
+			<?php
+				if ( $file ): ?>
+					<a target="_blank" class="button  button--light  vacancy__button"
+						href="<?php echo $file['url']; ?>">
+						<i class="fa fa-file-text-o"></i> Download this description
+					</a>
+			<?php endif; ?>
+
+			<?php
+				if ( $apply ): ?>
+				<a target="_blank" class="button  vacancy__button"
+					href="<?php echo $apply ?>">
+					<i class="fa fa-send"></i> Apply directly
+				</a>
+			<?php endif; ?>
+		</div>
+
+	</main>
+
+<?php
+	endwhile; endif;
+	get_footer();
+?>

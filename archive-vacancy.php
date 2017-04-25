253aef7b4f8d0f30caaae1925504484f389bc753
Floris Jansen <florismartijnjansen@gmail.com>
2017-04-25 11:38:25 +0200

    update layout for vacancies

--- archive-vacancy.php
+++ archive-vacancy.php
@@ -2,9 +2,11 @@
 
 <main>
 
-	<h1 class="archive__title"><?php echo esc_attr_x( 'Vacancies', 'archive title' ); ?></h1>
+	<h1 class="archive__title archive__title--vacancies">
+		<?php echo esc_attr_x( 'Vacancies', 'archive title' ); ?>
+	</h1>
 
-	<div class="vacancies">
+	<div class="vacancies vacancies--archive">
 		<?php while(have_posts()) : the_post(); ?>
 
 			<?php include( 'inc/small-vacancy.php' ); ?>

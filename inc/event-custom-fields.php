e841d1811e387095d5317c9ea051d51f7744f87a
Floris Jansen <florismartijnjansen@gmail.com>
2017-05-02 11:53:42 +0200

    add files to event (requires acf pro) (#22)

--- inc/event-custom-fields.php
+++ inc/event-custom-fields.php
@@ -100,3 +100,94 @@ if(function_exists("register_field_group"))
 		'menu_order' => 0,
 	));
 }
+
+
+
+// Event files (requires ACF Pro)
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+	'key' => 'group_590849a5a0993',
+	'title' => 'Event files',
+	'fields' => array (
+		array (
+			'key' => 'field_590849aac66df',
+			'label' => 'File list',
+			'name' => 'file_list',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => '',
+			'min' => 0,
+			'max' => 0,
+			'layout' => 'table',
+			'button_label' => '',
+			'sub_fields' => array (
+				array (
+					'key' => 'field_590849c9c66e0',
+					'label' => 'Name',
+					'name' => 'file_name',
+					'type' => 'text',
+					'instructions' => 'Displayed name of the file.',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array (
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+				),
+				array (
+					'key' => 'field_590849d8c66e1',
+					'label' => 'File',
+					'name' => 'file',
+					'type' => 'file',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array (
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'array',
+					'library' => 'all',
+					'min_size' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+			),
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'event',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;

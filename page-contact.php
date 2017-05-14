d801deb5bbaab340ccf2f7763cf6956e6a35048f
nwesthoff <nilsepils94@hotmail.com>
2017-05-14 12:48:50 +0200

    Revised lay-out and content for contact page

--- page-contact.php
+++ page-contact.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Template Name: contact page
@@ -18,34 +18,36 @@ get_header(); ?>
 <?php endif; ?> -->
 
 <section class="contact-page__container">
+		<h2 class="contact-page__subtitle">I have a question</h2>
 		<div class="contact-page__block">
-			<h3>Change address?</h3>
-			<p>You can call us any time during business hours.</p>
-			<a class="contact-page__info__button button button--call" href="tel:+31152783012"><i class="fa fa-phone"></i> Call us</a>
+			<h3>How do I change my address?</h3>
+			<p>If you'd like to change your address (or other contact info), please send our secretary an e-mail. Or do it yourself in our fancy new member area.</p>
+			<a class="contact-page__info__button button button--mail" href="mailto:svid@tudelft.nl"><i class="fa fa-envelope"></i> svid@tudelft.nl</a>
 		</div>
 		<div class="contact-page__block">
-			<h3>Add vacancy?</h3>
-			<p>If you'd rather send an e-mail, that's fine too.</p>
-			<a class="contact-page__info__button button button--mail" href="mailto:svid@tudelft.nl"><i class="fa fa-envelope"></i> Send e-mail</a>
+			<h3>Can I add a vacancy to your website?</h3>
+			<p>If you'd like your vacancy to be on our vacancy page, send our commissioner of external affairs an e-mail, if you prefer to call, that's fine as well.</p>
+			<a class="contact-page__info__button button button--mail" href="mailto:extern-svid@tudelft.nl"><i class="fa fa-envelope"></i> extern-svid@tudelft.nl</a>
+			<a class="contact-page__info__button button button--call" href="tel:+31(0)152783012"><i class="fa fa-envelope"></i> +31 (0)15 278 3012</a>
 		</div>
 		<div class="contact-page__block">
-			<h3>Feedback on education?</h3>
-			<p>Send us mail or come over at our facility.</p>
-			<p>
-				Landbergstraat 15 <br>
-				2628CE
-			</p>
-			<a class="contact-page__info__button button button--maps" target="blank" href="https://www.google.nl/maps/place/Studievereniging+i.d/@52.0019488,4.3678436,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b5930fa650eb:0xc234015f7f4f3fcf!8m2!3d52.0019455!4d4.3700323"><i class="fa fa-map-marker"></i> Open in Maps</a>
+			<h3>Where do I send feedback about education?</h3>
+			<p>If you have any feedback on education at IDE, be sure to check out the <a href="/education">education</a> page. You can send an e-mail with any questions as well.</p>
+			<a class="contact-page__info__button button button--mail" href="mailto:bachelor-svid@tudelft.nl"><i class="fa fa-envelope"></i> bachelor-svid@tudelft.nl</a>
+			<a class="contact-page__info__button button button--mail" href="mailto:master-svid@tudelft.nl"><i class="fa fa-envelope"></i> master-svid@tudelft.nl</a>
 		</div>
 		<div class="contact-page__block">
-			<h3>Other questions?</h3>
-			<p>We're on Facebook messenger, send us a message and we'll get back to you shortly.</p>
-			<p>/studieverenigingi.d</p>
-			<a class="contact-page__info__button button button--messenger" target="blank" href="https://m.me/studieverenigingi.d"><i class="fa fa-comment"></i> Send message</a>
+			<h3>I have another question.</h3>
+			<p>If you have any other questions, there's plenty of ways to reach us.</p>
+			<a class="contact-page__info__button button button--mail" href="mailto:svid@tudelft.nl"><i class="fa fa-envelope"></i> svid@tudelft.nl</a>
+			<a class="contact-page__info__button button button--call" href="tel:+31(0)152783012"><i class="fa fa-envelope"></i> +31 (0)15 278 3012</a>
+			<a class="contact-page__info__button button button--messenger" target="blank" href="https://m.me/studieverenigingi.d"><i class="fa fa-comment"></i> /studieverenigingi.d</a>
 		</div>
+</section>
 
-	<!-- <h2>I want to stalk you on social media</h2>
-	<div>
+<section class="contact-page__container">
+	<h2 class="contact-page__subtitle">I want to stalk you on social media</h2>
+	<div class="contact-page__block">
 		<a href="https://www.instagram.com/studieverenigingid/"
 			class="button button--insta">
 			<i class="fa fa-instagram"></i> Instagram
@@ -63,24 +65,30 @@ get_header(); ?>
 			<i class="fa fa-vimeo"></i> Vimeo
 		</a>
 	</div>
+</section>
 
-	<h2>Opening Hours</h2>
-	<p>Study association i.d is opened on:</p>
-	<ul>
-		<li>Monday: 9:00 - 17:00</li>
-		<li>Tuesday: 9:00 - 17:00</li>
-		<li>Wednesday: 12:30 - 17:00</li>
-		<li>Thursday: 9:00 - 17:00</li>
-		<li>Friday: 9:00 - 17:00</li>
-	</ul>
+<section class="contact-page__container">
+	<h2 class="contact-page__subtitle">Opening Hours</h2>
+	<div class="contact-page__block">
+		<p>Study association i.d is opened on:</p>
+		<ul>
+			<li>Monday: 9:00 - 17:00</li>
+			<li>Tuesday: 9:00 - 17:00</li>
+			<li>Wednesday: 12:30 - 17:00</li>
+			<li>Thursday: 9:00 - 17:00</li>
+			<li>Friday: 9:00 - 17:00</li>
+		</ul>
+	</div>
 
-	<h2>Other information</h2>
-	<ul>
-		<li>KVK: Haaglanden V 40397069</li>
-		<li>BTW: NL 8058.24.352 B01</li>
-		<li>IBAN: NL 08 RABO 0319423239</li>
-		<li>RABONL2U</li>
-	</ul>-->
+	<h2 class="contact-page__subtitle">Other information</h2>
+	<div class="contact-page__block">
+		<ul>
+			<li>KVK: Haaglanden V 40397069</li>
+			<li>BTW: NL 8058.24.352 B01</li>
+			<li>IBAN: NL 08 RABO 0319423239</li>
+			<li>RABONL2U</li>
+		</ul>
+	</div>
 </section>
 
 <?php get_footer(); ?>

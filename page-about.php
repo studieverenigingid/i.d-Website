94992892bbe99ee6f1b75ccdd208c53fe92362fc
Floris Jansen <florismartijnjansen@gmail.com>
2017-05-15 19:51:43 +0200

    add base structure for about page

---
+++ page-about.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+
+
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<main class="about__top">
+	<h1 class="about__title"><?php the_title(); ?></h1>
+	<p class="about__descr"><?php echo get_the_content(); ?></p>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<?php the_post_thumbnail('post-thumbnail',
+			array( 'class' => 'about__thumb')
+		); ?>
+	<?php endif; ?>
+</main>
+<?php endwhile; endif; ?>
+
+
+
+<section class="board"></section>
+
+
+
+<section class="committees"></section>
+
+
+
+<section class="hon-mems"></section>
+
+
+
+<?php get_footer(); ?>

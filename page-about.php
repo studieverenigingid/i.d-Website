b3b8528ce02b598e6befc48d2e06aba3eafe5648
Floris Jansen <florismartijnjansen@gmail.com>
2017-05-20 17:39:18 +0200

    update committee layout

--- page-about.php
+++ page-about.php
@@ -30,7 +30,7 @@
 		<p class="board__indication">
 			<?php echo esc_attr_x('The current board', 'shows above the board title'); ?>
 		</p>
-		<h2 class="board__title">
+		<h2 class="about__sub-title">
 			<?php the_title(); ?>
 		</h2>
 
@@ -52,24 +52,54 @@
 
 
 <section class="committees">
+
+	<h2 class="about__sub-title">Our committees</h2>
+
+	<div class="committees__grid">
 	<?php
 		wp_reset_postdata();
-		$args = array( 'post_type' => 'committee', 'posts_per_page' => 30 );
-		$loop = new WP_Query( $args );
-    if($loop->have_posts()) : ?>
 
-		<h2>Our committees</h2>
+		// Get groups/types of committees
+		$groups = get_terms( array(
+    	'taxonomy' => 'committee-group',
+    	'hide_empty' => true,
+		) );
 
-		<div class="committees__grid">
-			<?php while($loop->have_posts()) : $loop->the_post(); ?>
-				<article class="committee">
-					<h3 class="committee__name"><?php the_title(); ?></h3>
-				</article>
-			<?php
-				endwhile; endif;
-				wp_reset_postdata();
-			?>
-		</div>
+		// Cycle through those types and spawn
+		foreach ($groups as $key => $group) {
+			$args = array(
+				'post_type' => 'committee',
+				'posts_per_page' => 30,
+				'tax_query' => array(
+        	array(
+            'taxonomy' => 'committee-group',
+            'field' => 'slug',
+            'terms' => array($group->slug)
+        	)
+    		)
+			);
+
+			$group_class = 'committee--'.$group->slug;
+			$group_name = $group->name;
+
+			$loop = new WP_Query( $args );
+	    if($loop->have_posts()) : ?>
+
+			<article class="committee committee--group <?=$group_class?>">
+				<h3 class="committee--group__name"><?=$group_name?></h3>
+			</article>
+
+				<?php while($loop->have_posts()) : $loop->the_post(); ?>
+					<article class="committee">
+						<h4 class="committee__name"><?php the_title(); ?></h4>
+					</article>
+				<?php
+					endwhile; endif;
+					wp_reset_postdata();
+				?>
+		<?php } ?>
+
+	</div>
 
 </section>
 

ae08f569f6e6332750e6f6b70ca9ed0dce566640
nwesthoff <nilsepils94@hotmail.com>
2017-05-23 11:00:40 +0200

    Custom Report-i.d blog category page

---
+++ category-reportid.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<main>
+
+	<h1 class="archive__title"><?php single_cat_title(); ?></h1>
+
+	<div class="news">
+
+	<?php
+		$loop = new WP_Query( $args );
+		if(have_posts()) : while(have_posts()) :
+			the_post();
+			include 'inc/small-news-item.php';
+		endwhile; endif;
+
+		wp_reset_postdata();
+	?>
+
+	</div>
+
+</main>
+
+<?php include 'inc/pagination.php'; ?>
+
+<?php get_footer(); ?>

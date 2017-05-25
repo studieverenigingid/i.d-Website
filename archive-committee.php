03f0c59c09ab8918504a5d92ce25a85c3704ec49
Floris Jansen <florismartijnjansen@gmail.com>
2017-05-25 18:31:34 +0200

    add overview/archive page for committees

---
+++ archive-committee.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<main>
+
+	<h1 class="archive__title"><?php echo esc_attr_x('Our committees'); ?></h1>
+
+	<div class="committees__grid">
+
+	<?php
+		if(have_posts()) : while(have_posts()) :
+			the_post();
+			$groups = get_the_terms($post, 'committee-group');
+			$group_class = 'comm-group--'.$groups[0]->slug;
+			include 'inc/small-committee.php';
+		endwhile; endif;
+
+		wp_reset_postdata();
+	?>
+
+	</div>
+
+</main>
+
+<?php include 'inc/pagination.php'; ?>
+
+<?php get_footer(); ?>

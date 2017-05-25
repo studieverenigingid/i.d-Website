81be2a015c07a56c1c13e63936e6b6e08f2c24ed
Floris Jansen <florismartijnjansen@gmail.com>
2017-05-25 18:16:33 +0200

    make committee clickable

--- page-about.php
+++ page-about.php
@@ -91,10 +91,13 @@
 
 			<?php while($loop->have_posts()) : $loop->the_post(); ?>
 				<article class="committee">
-					<div class="committee__thumb <?=$group_class?>"
-						style="background-image: url(<?=the_post_thumbnail_url('thumb')?>)"
-					></div>
-					<h4 class="committee__name"><?php the_title(); ?></h4>
+					<a class="committee__link"
+						href="<?php the_permalink(); ?>">
+						<div class="committee__thumb <?=$group_class?>"
+							style="background-image: url(<?=the_post_thumbnail_url('thumb')?>)">
+						</div>
+						<h4 class="committee__name"><?php the_title(); ?></h4>
+					</a>
 					<?php the_excerpt(); ?>
 				</article>
 			<?php

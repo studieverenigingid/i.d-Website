520881e85b2e21dc31e77166305ffb50d1ef03cb
nwesthoff <nilsepils94@hotmail.com>
2017-05-28 12:00:17 +0200

    Turn The Page cover is clickable too

--- inc/small-turnthepage.php
+++ inc/small-turnthepage.php
@@ -1,5 +1,5 @@
 <article class="ttp-issue" style="background-color:<?php the_field('issue_background_color');?>;">
-	<?php if ( has_post_thumbnail() ) : ?><?php the_post_thumbnail('medium_large', array( 'class' => 'ttp-issue__thumb')); ?><?php endif; ?>
+	<?php if ( has_post_thumbnail() ) : ?><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium_large', array( 'class' => 'ttp-issue__thumb')); ?></a><?php endif; ?>
 	<div class="ttp-issue__meta">
 		<div class="ttp-issue__number">Issue #<?php the_field('issue_number');?></div>
 		<h3 class="ttp-issue__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>

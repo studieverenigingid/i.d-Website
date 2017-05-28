71c261f06ea85ecc0133d6a8c878e8c95750557c
nwesthoff <nilsepils94@hotmail.com>
2017-05-28 13:16:43 +0200

    remove date from single-board.php

--- single-board.php
+++ single-board.php
@@ -10,25 +10,6 @@
 
 		<h1 class="news-item__title--large"><?php the_title(); ?></h1>
 
-		<div class="news-item__meta--large">
-			<?php $parentscategory = "";
-				$has_cats = false;
-				foreach((get_the_category()) as $category) {
-					if ($category->category_parent == 0) {
-						$cat_link = get_category_link($category->cat_ID);
-						$cat_name = $category->name;
-						$parentscategory .= ' <a ' .
-							'href="' . $cat_link . '"' .
-							'class="news-item__category"' .
-							'title="' . $cat_name . '">' .
-							$cat_name . '</a>, ';
-						$has_cats = true;
-					}
-				}
-				if ($has_cats) echo substr($parentscategory,0,-2) . ' | '; ?>
- 			<?php echo get_the_date(); ?>
-		</div>
-
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail('post-thumbnail',
 				array( 'class' => 'news-item__thumb--large')

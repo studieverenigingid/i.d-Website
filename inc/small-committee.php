02fa5d02f9fc657a7528b79fabe13d1c84545077
nwesthoff <nilsepils94@hotmail.com>
2017-06-13 10:51:38 +0200

    Committee archive uses medium thumbnails

--- inc/small-committee.php
+++ inc/small-committee.php
@@ -2,7 +2,7 @@
 	<a class="committee__link"
 		href="<?php the_permalink(); ?>">
 		<div class="committee__thumb <?=$group_class?>"
-			style="background-image: url(<?=the_post_thumbnail_url('thumb')?>)">
+			style="background-image: url(<?=the_post_thumbnail_url('medium')?>)">
 		</div>
 		<h4 class="committee__name"><?php the_title(); ?></h4>
 	</a>

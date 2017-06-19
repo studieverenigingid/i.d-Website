1c0e178983e2b6f3004e11bae75913b16745c053
Floris Jansen <florismartijnjansen@gmail.com>
2017-06-19 22:51:01 +0200

    add favicon code

--- header.php
+++ header.php
@@ -13,8 +13,13 @@ global $header_class;
 		} elseif (is_post_type_archive('board') || is_singular('board')) {
 			?> content="<?php the_field('board_color');?>"<?php
 		}else { echo 'content="#55ccbb"';
-		}?>><?php /* TODO:
-		replace with realfavicongenerator.net snippet */ ?>
+		}?>>
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/manifest.json">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#55ccbb">
 
 		<?php
 			if ( ! function_exists( '_wp_render_title_tag' ) ) :

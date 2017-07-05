13b0aa6e77f6bd2d9ff4e1a31b995585531ead70
nwesthoff <nilsepils94@hotmail.com>
2017-07-05 20:30:25 +0200

    user page created, successfully pulls data from Lassie

---
+++ page-user.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Template Name: User Profile Page
+ */
+  if (!is_user_logged_in()) {
+    header('Location: '. wp_login_url( get_permalink()) );
+  } else {
+	get_header();
+?>
+
+	<main class="primary-content news--page__content">
+
+		<?php
+
+        $person = Lassie::getPerson();
+        echo $person->first_name." ".$person->last_name."<br />";
+        echo $person->email_primary."<br />";
+        echo $person->birthdate."<br />";
+        echo $person->phone_mobile."<br />";
+        echo $person->address_street." ".$person->address_number."<br />";
+        echo $person->address_zip."<br />";
+        echo $person->address_country."<br />";
+
+    ?>
+
+	</main>
+
+<?php
+	get_footer();
+  }
+?>

6909993d0d9241c79dd3ae11a84fa976fdff9aac
nwesthoff <nilsepils94@hotmail.com>
2017-08-01 18:01:56 +0200

    moved userInfoEdit function to separate userinfoedit.js file

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,8 @@ function onDocReady () {
 	ajaxFeedbackForm();
 	hideUpdateFields();
 	resetFormLink();
-	// user form handling
+
+	// userinfoedit.js
 	userInfoEdit();
 
 	// social.js
@@ -27,55 +28,6 @@ function menuToggler () {
 		menuToggle.toggleClass('opened');
 	});
 
-}
-
-function userInfoEdit() {
-
-	$('.user__info__edit--edit').on('click', function () {
-	    $('.user__info__input--editable').attr('readonly', false).each(function () {
-	        var $this = $(this);
-	        $this.attr('data-before', $this.val());
-	    });
-	    $('.user__info__edit--edit, .user__info__edit--save, .user__info__edit--cancel').toggleClass('hidden');
-	});
-
-  $('.user__info__edit--save').on('click', function () {
-  	formReadonly();
-  });
-
-  $('.user__info__edit--cancel').on('click', function () {
-      $('.user__info__input--editable').each(function () {
-          var $this = $(this);
-          $this.val($this.attr('data-before')).removeClass('invalid');
-      });
-
-			formReadonly();
-  });
-
-	function formReadonly() {
-		$('.user__info__input--editable').attr('readonly', true);
-		$('.user__info__edit--edit, .user__info__edit--save, .user__info__edit--cancel').toggleClass('hidden');
-	}
-
-	$(document).on('submit' , 'form.user__info', function(e) {
-
-		e.preventDefault();
-
-		var form = $(this);
-
-		var data = form.serialize();
-		data += '&submit=true&action=user_update';
-
-		$.ajax({
-			url: wpjs_object.ajaxurl,
-			type: 'POST',
-			dataType: 'json',
-			data: data,
-		}).done(function(data) {
-			console.log(data);
-		});
-
-	})
 }
 
 function fixVHAfterLoad() {

7d7de161a55c1497bae1c1242613ac892cd9c4b9
Floris Jansen <florismartijnjansen@gmail.com>
2017-08-12 21:59:12 +0200

    add titles in archive between past and upcoming events

--- archive-event.php
+++ archive-event.php
@@ -5,17 +5,31 @@
 	<h1 class="archive__title"><?php echo esc_attr_x( 'Events', 'archive title' ); ?></h1>
 
 	<div class="events">
+
+		<h2 class="events__archive-title" style="order: -100;">
+			<?php echo esc_attr_x( 'Upcoming events', 'archive' ); ?>
+		</h2>
+
 		<?php
 			$current_date = new DateTime();
 			$current_date->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+			$first_past = true;
 			$post_no = 0;
 		?>
+
 		<?php while(have_posts()) : the_post(); ?>
 
 			<?php
 				$start = new DateTime(get_field('start_datetime'));
 				$start->setTimezone( new DateTimeZone('Europe/Amsterdam') );
+				if ($start < $current_date && $first_past):
+					$first_past = false;
 			?>
+				<h2 class="events__archive-title">
+					<?php echo esc_attr_x( 'Past events', 'archive' ); ?>
+				</h2>
+			<?php endif; ?>
+
 			<?php
 				// Add a order to upcoming events to sort them reverse from how we got them
 				if ($start > $current_date) {

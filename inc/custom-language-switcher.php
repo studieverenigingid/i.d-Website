a2d4fbf6e00b5daa43517f31d5011bddc5e03be2
nwesthoff <nilsepils94@hotmail.com>
2017-09-02 00:47:46 +0200

    Language switcher got style

--- inc/custom-language-switcher.php
+++ inc/custom-language-switcher.php
@@ -4,11 +4,15 @@
       if(!empty($languages)){
           echo '<ul class="primary-menu__language-switch">';
           foreach($languages as $l){
-              echo '<li class="menu-item menu-item--language-switch">';
-                if(!$l['active']) echo '<a href="'.$l['url'].'">';
-                  echo strtoupper($l['language_code']);
-                if(!$l['active']) echo '</a>';
-              echo '</li>';
+              echo '';
+                if(!$l['active']){?>
+                  <li class="menu-item menu-item--language-switch">
+                    <span class="primary-menu__language-switch__context"><?php echo esc_attr_x( 'Switch to: ', 'Language Switch Text' ); ?></span>
+                    <a class="primary-menu__language-switch__link" href="<?=$l['url']?>">
+                      <?=$l['language_code']?>
+                    </a>
+                  </li>
+              <?php }
           }
           echo '</ul>';
       }

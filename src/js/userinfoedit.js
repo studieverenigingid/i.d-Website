d5814dd18bbf1c90c4a15799fad07c1542862f6d
Floris Jansen <florismartijnjansen@gmail.com>
2017-10-04 18:21:14 +0300

    use feedback from server to display messages in notifications

--- src/js/userinfoedit.js
+++ src/js/userinfoedit.js
@@ -1,42 +1,49 @@
 function userInfoEdit() {
 
 	$('.user__info__edit--edit').on('click', function () {
-	    $('.user__info__input--editable').attr('readonly', false).each(function () {
-	        var $this = $(this);
-	        $this.attr('data-before', $this.val());
-	    });
-	    $('.user__info__edit--edit, .user__info__edit--save, .user__info__edit--cancel').toggleClass('hidden');
+		$('.user__info__input--editable').attr('readonly', false).each(function () {
+			var $this = $(this);
+			$this.attr('data-before', $this.val());
+		});
+		$('.user__info__edit--edit, .user__info__edit--save, .user__info__edit--cancel').toggleClass('hidden');
 	});
 
-  $('.user__info__edit--save').on('click', function () {
-  	formReadonly();
-  });
+	$('.user__info__edit--cancel').on('click', function () {
+		$('.user__info__input--editable').each(function () {
+			var $this = $(this);
+			$this.val($this.attr('data-before')).removeClass('invalid');
+		});
 
-  $('.user__info__edit--cancel').on('click', function () {
-      $('.user__info__input--editable').each(function () {
-          var $this = $(this);
-          $this.val($this.attr('data-before')).removeClass('invalid');
-      });
-
-			formReadonly();
-  });
+		formReadonly();
+	});
 
 	function formReadonly() {
 		$('.user__info__input--editable').attr('readonly', true);
 		$('.user__info__edit--edit, .user__info__edit--save, .user__info__edit--cancel').toggleClass('hidden');
 	}
 
-	function formDone(response) {
-		console.log(response);
+	function formDone(response, status, error) {
+		if (!response['success']) {
+			formFail(response, status, error);
+			return;
+		}
+
+		formReadonly();
+
+		$('.user__info__column--right').prepend('<h4 class="notification notification--success">' + response['message'] + '</h4>');
 	}
 
-	function formFail(response) {
-		console.log(response);
+	function formFail(response, status, error) {
+		$('.user__info__column--right').prepend('<h4 class="notification notification--failed">' + response['message'] + '</h4>');
+		console.log(error);
+		console.log(error === 'net::ERR_INTERNET_DISCONNECTED');
 	}
 
 	$(document).on('submit' , 'form.user__info', function(e) {
 
 		e.preventDefault();
+
+		$('.notification').remove();
 
 		var formData = new FormData($(this)[0]);
 
@@ -47,10 +54,8 @@ function userInfoEdit() {
 			data: formData,
 			processData: false,
 			contentType: false
-		}).done(
-			formDone()
-		).fail(
-			formFail()
-		);
+		})
+		.done(formDone)
+		.fail(formFail);
 	})
 }

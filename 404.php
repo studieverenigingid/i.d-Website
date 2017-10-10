1203929bed72b612325ed153138b47e7fdafa894
Floris Jansen <florismartijnjansen@gmail.com>
2017-10-10 23:13:24 +0300

    include 404 page in theme_color

--- 404.php
+++ 404.php
@@ -2,7 +2,8 @@
 	get_header();
 ?>
 
-<main class="not-found">
+<main class="not-found"
+	style="background-color: <?php theme_color(false); ?>;">
 	<picture>
 		<source srcset="<?=$img_folder?>404.svg" type="image/svg+xml">
 		<img class="not-found__image" alt="?"

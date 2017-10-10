52e0d6fd8196fd04a4fd028f3c65f6bd4cfa1e0f
Floris Jansen <florismartijnjansen@gmail.com>
2017-10-10 20:07:37 +0300

    always give events a page color based on the post thumbnail

--- inc/color-custom-fields.php
+++ inc/color-custom-fields.php
@@ -20,7 +20,7 @@ acf_add_local_field_group(array (
 				'class' => '',
 				'id' => '',
 			),
-			'default_value' => '#55ccbb',
+			'default_value' => '',
 		),
 	),
 	'location' => array (
@@ -76,5 +76,38 @@ acf_add_local_field_group(array (
 	'active' => 1,
 	'description' => '',
 ));
+
+add_action( 'save_post', 'event_image_color' );
+
+function event_image_color($post_id) {
+
+	$post_type = get_post_type($post_id);
+
+	// If this isn't an event, don't update it.
+	if ( 'event' != $post_type ) return;
+
+	// If the page color is custom set, don’t update it.
+	if ( '' != get_field('page_color') ) return;
+
+	// If there is no post thumbnail, we can’t extract color; don’t update it.
+	if ( !has_post_thumbnail() ) return;
+
+	$thumbnail_url = get_the_post_thumbnail_url($post_id, 'thumb');
+
+	include_once('colors.inc.php');
+
+	$get_colors = new GetMostCommonColors();
+
+	$colors = $get_colors->Get_Color(
+		$thumbnail_url, // image url
+		10, 1, 1, 30 // 6 colors, reduce brightness, reduce gradients, delta of 24
+	);
+
+	end($colors);
+	$new_color = '#' . key($colors);
+
+	update_field('page_color', $new_color, $post_id);
+
+}
 
 endif;

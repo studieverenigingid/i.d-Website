c9dff6b3457ba9263fe293eb777a6cf8afeac346
Floris Jansen <florismartijnjansen@gmail.com>
2017-12-17 16:46:52 +0200

    add exception for non-Lassie users or those which lack a username

--- inc/samltud-helper.php
+++ inc/samltud-helper.php
@@ -20,6 +20,7 @@ class SAMLTUD_Helper {
    */
 	public function save_netid( $user_login, $user ) {
 		$netid = $this->getUsername($user);
+		if ( !$netid ) return;
 		if ( !$this->has_netid($user->ID) ) {
 			add_user_meta( $user->ID, 'svid_netid', $netid);
 		}

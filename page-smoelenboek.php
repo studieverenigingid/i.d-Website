9db6a9b24ed0564828e4714936780ec0017e433c
Floris Jansen <florismartijnjansen@gmail.com>
2017-12-23 22:45:28 +0100

    redo base of smoelenboek

--- page-smoelenboek.php
+++ page-smoelenboek.php
@@ -3,17 +3,17 @@
 /**
  * Template Name: Smoelenboek
  */
-  if (!is_user_logged_in()) {
-    header('Location: '. get_home_url(null, 'login'));
-  } else {
-	get_header();
+if (!is_user_logged_in()):
+  header('Location: '. get_home_url(null, 'login'));
+else:
+  get_header();
 ?>
 
-<div class="person-search">
-  <input class="person-search__searchbar" placeholder="Search user..." value="">
-</div>
+<main class="smoelenboek__top">
+  <h1 class="smoelenboek__title">Smoelenboek</h1>
+</main>
 
 <?php
 	get_footer();
-  }
+endif;
 ?>

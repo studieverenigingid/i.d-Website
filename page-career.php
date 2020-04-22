3138617313da4357ad9fc876c4bca9e182029c20
Floris Jansen <florismartijnjansen@gmail.com>
2020-04-22 17:38:37 +0200

    create career overview page (fix #51)

---
+++ page-career.php
@@ -0,0 +1,207 @@
+<?php
+
+/**
+ * Template Name: Career page
+ */
+
+get_header(); ?>
+
+
+
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<header id="site-content" class="kafee--page__header
+	<?php if ( !has_post_thumbnail() ) echo 'kafee--page__header--short-header'; ?>"
+	style="background-color: <?php theme_color(false); ?>;">
+
+	<div class="kafee--page__short-info
+		<?php if ( !has_post_thumbnail() ) echo 'kafee--page__short-info--short-header'; ?>"
+		style="background-color: <?php theme_color(false); ?>;">
+
+		<h1 class="kafee--page__name"><?php the_title(); ?></h1>
+
+      <div class="kafee--page__kafee">
+        <?php the_content(); ?>
+      </div>
+
+		<?php if ( has_post_thumbnail() ) : ?>
+			<div class="kafee--page__thumb">
+				<?php
+				the_post_thumbnail(
+					'large',
+					array('class' => 'kafee--page__img')
+				);
+				?>
+			</div>
+		<?php endif; ?>
+
+	</div>
+
+</header>
+<?php endwhile; endif; ?>
+
+
+
+<p class="career__jump" style="background-color: <?php theme_color(false); ?>;">
+	Jump to our:
+	<a class="about__jump-link" href="#partner-companies">
+		<?php echo esc_attr_x('partner companies',
+			'Partner companies link',
+			'svid-theme-domain'); ?></a> –
+	<a class="about__jump-link" href="#vacancies">
+		<?php echo esc_attr_x('vacancies',
+			'Vacancies link',
+			'svid-theme-domain'); ?></a> –
+	<a class="about__jump-link" href="#collaboration">
+		<?php echo esc_attr_x('collaborate with ID',
+			'Collaboration link',
+			'svid-theme-domain'); ?></a> –
+	<a class="about__jump-link" href="#alumni">
+		<?php echo esc_attr_x('alumni',
+			'Alumni link',
+			'svid-theme-domain'); ?></a>
+</p>
+
+
+
+
+<section class="career__partners" id="partner-companies">
+
+	<h2 class="about__sub-title"><?php echo esc_attr_x('Partner companies', 'Our committees title', 'svid-theme-domain'); ?></h2>
+
+	<?php if(get_field('about_partners')): ?>
+	<div class="career__description">
+		<?php the_field('about_partners'); ?>
+	</div>
+	<?php endif; ?>
+
+	<?php
+		$partner_page_ID = get_page_by_path('partner-companies');
+		if( have_rows('companies', $partner_page_ID) ): $i = 0; ?>
+			<section class="partner-companies partner-companies--career">
+		 	<?php // loop through the rows of data
+		    while ( have_rows('companies', $partner_page_ID) ) : the_row();
+				$i++; if($i > 4) break; ?>
+					<?php
+						$logo = get_sub_field('logo');
+						$logo = $logo['sizes']['medium'];
+					?>
+					<div class="partner-company__logo-bubble partner-company__logo-bubble--career">
+						<img class="partner-company__logo" src="<?php echo $logo; ?>"
+							alt="Logo <?php the_sub_field('name'); ?>">
+					</div>
+		    <?php endwhile; ?>
+			</section>
+		<?php else :
+			echo "no partner companies found";
+		endif;
+	?>
+
+	<a class="button committees__all"
+		href="<?php echo get_permalink( $partner_page_ID ); ?>">
+		<?php echo esc_attr_x('All partners', 'View all partner companies button text', 'svid-theme-domain'); ?>
+	</a>
+
+</section>
+
+
+
+<hr class="divider">
+
+
+
+<section class="career__vacancies" id="vacancies">
+
+	<h2 class="about__sub-title"><?php echo esc_attr_x('Looking for a job or internship?', 'Vacancies title', 'svid-theme-domain'); ?></h2>
+
+	<?php if(get_field('about_vacancies')): ?>
+	<div class="career__description">
+		<?php the_field('about_vacancies'); ?>
+	</div>
+	<?php endif; ?>
+
+	<div class="career__small-vacancy">
+
+		<h3 class="career__small-heading">Most recent vacancy</h3>
+
+		<?php
+			$args = array(
+				'post_type' => 'vacancy',
+				'posts_per_page' => 1
+			);
+
+			$loop = new WP_Query( $args );
+	    if($loop->have_posts()) :
+				while($loop->have_posts()) :
+					$loop->the_post();
+					include 'inc/small-vacancy.php';
+				endwhile;
+			endif;
+			wp_reset_postdata();
+		?>
+
+	</div>
+
+	<a class="button committees__all"
+		href="<?php echo get_post_type_archive_link( 'vacancy' ); ?>">
+		<?php echo esc_attr_x('All vacancies', 'View all vacancies button text', 'svid-theme-domain'); ?>
+	</a>
+
+</section>
+
+
+
+<hr class="divider">
+
+
+
+<section class="career__collaboration" id="collaboration">
+	<h2 class="about__sub-title">
+		<?php echo esc_attr_x('Collaborate with ID', 'Coll title', 'svid-theme-domain'); ?>
+	</h2>
+
+	<?php if(get_field('about_collaboration')): ?>
+	<div class="career__description">
+		<?php the_field('about_collaboration'); ?>
+	</div>
+	<?php endif; ?>
+
+	<?php
+	$collab_page_ID = get_page_by_path('collaborate');
+	?>
+	<a class="button committees__all"
+		href="<?php echo get_permalink( $collab_page_ID ); ?>">
+		<?php echo esc_attr_x('About collaborations', 'Collaboration page button text', 'svid-theme-domain'); ?>
+	</a>
+
+</section>
+
+
+
+<hr class="divider">
+
+
+
+<section class="career__alumni" id="alumni">
+	<h2 class="about__sub-title">
+		<?php echo esc_attr_x('About our alumni', 'Alumni title', 'svid-theme-domain'); ?>
+	</h2>
+
+	<?php if(get_field('about_alumni')): ?>
+	<div class="career__description">
+		<?php the_field('about_alumni'); ?>
+	</div>
+	<?php endif; ?>
+
+	<?php
+	$alumni_page_ID = get_page_by_path('alumni');
+	?>
+	<a class="button committees__all"
+		href="<?php echo get_permalink( $alumni_page_ID ); ?>">
+		<?php echo esc_attr_x('Read about alumni', 'Collaboration page button text', 'svid-theme-domain'); ?>
+	</a>
+
+</section>
+
+
+
+<?php get_footer(); ?>

207cf8406c5d9ac055a135a3f1f14e972fa7e8f2
Floris Jansen <florismartijnjansen@gmail.com>
2020-06-25 16:34:04 +0200

    switch feedback form and news places

--- education-page.php
+++ education-page.php
@@ -13,82 +13,25 @@ wp_enqueue_script( 'recaptcha', 'https://www.google.com/recaptcha/api.js' );
 	<h1 class="education__title">
     <?php the_title(); ?>
   </h1>
-	<div class="education-feedback">
-    <div class="education-feedback__message education-feedback__message--success">
-      <?= esc_attr_x('Your input was sent, thanks!', 'feedback-form-message', 'svid-theme-domain')?><br>
-			<a href="#reset" class="education-feedback__link js-reset-link"><?php echo esc_attr_x('I have even more feedback.', 'More feedback label text', 'svid-theme-domain');?></a>
-    </div>
-
-		<form action="#" class="education-feedback__wrap">
-
-        <label for="feedback" class="education__label">
-          <?= esc_attr_x('Do you have feedback about the faculty of IDE or your education?', 'feedback-form-question', 'svid-theme-domain')?>
-        </label>
-				<textarea name="feedback" id="" cols="30" rows="12"
-          placeholder="<?= esc_attr_x('I love course PO5 because...', 'feedback-form-placeholder', 'svid-theme-domain') ?>"
-          style="background-color: <?php theme_color(false); ?>"
-          class="education__input-message"
-          required></textarea>
-
-        <div class="education__updater">
-          <p class="education__label education__label--right">
-            <?= esc_attr_x('Do you want to be updated about this input?', 'feedback-form', 'svid-theme-domain')?>
-          </p>
-          <input type="checkbox" name="update" value="true"
-            class="js-edu-checkbox education__toggle">
-          <label for="update"
-            class="education__toggle-button js-edu-toggle"></label>
-        </div>
-
-        <div class="education__hideble-fields js-edu-hidable-fields">
-
-					<?php
-						if (is_user_logged_in()) {
-							$current_user = wp_get_current_user();
-							$current_name = $current_user->display_name;
-							$current_email = $current_user->user_email;
-						} else {
-							$current_name = '';
-							$current_email = '';
-						}
-				 	?>
-
-          <label for="feedback" class="education__label">
-            <?= esc_attr_x('What is your name?', 'feedback-form-question', 'svid-theme-domain')?>
-          </label>
-  				<input name="name" type="text" class="education__input-short"
-            placeholder="<?= esc_attr_x('Jamie Doe', 'feedback-form-placeholder', 'svid-theme-domain') ?>"
-						style="background-color: <?php theme_color(false); ?>"
-						value="<?=$current_name?>">
-
-          <label for="feedback" class="education__label">
-            <?= esc_attr_x('And your email address?', 'feedback-form-question', 'svid-theme-domain')?>
-          </label>
-  				<input type="email" name="email" class="education__input-short"
-            placeholder="<?= esc_attr_x('jamie@doe.com', 'feedback-form-placeholder', 'svid-theme-domain') ?>"
-						style="background-color: <?php theme_color(false); ?>"
-						value="<?=$current_email?>">
-
-        </div>
 
 
-        <div class="education__validate-and-send">
-
-          <input type="hidden" name="action" value="education_input">
-
-          <div class="g-recaptcha"
-            data-sitekey="6Ld7pCUUAAAAAFY2ezdhFaWW25L_c254ali_Hpsg">
-          </div>
-
-          <button type="submit" class="button education__submit">
-            <?= esc_attr_x('Send input', 'feedback-form-button', 'svid-theme-domain')?>
-          </button>
-
-        </div>
-
-		</form>
-	</div>
 </main>
+
+
+
+<?php
+$args = array( 'post_type' => 'post', 'category_name' => 'education', 'posts_per_page' => 3 );
+$loop = new WP_Query( $args );
+if($loop->have_posts()) : ?>
+  <section class="news education-news">
+    <h2><?= esc_attr_x('What is happening in education?', 'education-what-we-did-with-it', 'svid-theme-domain')?></h2>
+    <?php while($loop->have_posts()) : $loop->the_post(); ?>
+
+		<?php include('inc/small-news-item.php') ?>
+
+    <?php endwhile; ?>
+  </section>
+<?php endif; wp_reset_postdata(); ?>
 
 
 
@@ -123,6 +66,84 @@ if($loop->have_posts()) : ?>
     <?php endwhile; ?>
   </section>
 <?php endif; wp_reset_postdata(); ?>
+	<section class="education-feedback">
+		<h2><?= esc_attr_x('Orange notes', 'education-what-we-do-with-it', 'svid-theme-domain')?></h2>
+
+		<div class="education-feedback__message education-feedback__message--success">
+			<?= esc_attr_x('Your input was sent, thanks!', 'feedback-form-message', 'svid-theme-domain')?><br>
+			<a href="#reset" class="education-feedback__link js-reset-link"><?php echo esc_attr_x('I have even more feedback.', 'More feedback label text', 'svid-theme-domain');?></a>
+		</div>
+
+		<form action="#" class="education-feedback__wrap"
+			style="background-color: <?php theme_color(false); ?>">
+
+				<label for="feedback" class="education__label">
+					<?= esc_attr_x('Do you have feedback about the faculty of IDE or your education?', 'feedback-form-question', 'svid-theme-domain')?>
+				</label>
+				<textarea name="feedback" id="" cols="30" rows="12"
+					placeholder="<?= esc_attr_x('I love course PO5 because...', 'feedback-form-placeholder', 'svid-theme-domain') ?>"
+					style="background-color: <?php theme_color(false); ?>"
+					class="education__input-message"
+					required></textarea>
+
+				<div class="education__updater">
+					<p class="education__label education__label--right">
+						<?= esc_attr_x('Do you want to be updated about this input?', 'feedback-form', 'svid-theme-domain')?>
+					</p>
+					<input type="checkbox" name="update" value="true"
+						class="js-edu-checkbox education__toggle">
+					<label for="update"
+						class="education__toggle-button js-edu-toggle"></label>
+				</div>
+
+				<div class="education__hideble-fields js-edu-hidable-fields">
+
+					<?php
+						if (is_user_logged_in()) {
+							$current_user = wp_get_current_user();
+							$current_name = $current_user->display_name;
+							$current_email = $current_user->user_email;
+						} else {
+							$current_name = '';
+							$current_email = '';
+						}
+					?>
+
+					<label for="feedback" class="education__label">
+						<?= esc_attr_x('What is your name?', 'feedback-form-question', 'svid-theme-domain')?>
+					</label>
+					<input name="name" type="text" class="education__input-short"
+						placeholder="<?= esc_attr_x('Jamie Doe', 'feedback-form-placeholder', 'svid-theme-domain') ?>"
+						style="background-color: <?php theme_color(false); ?>"
+						value="<?=$current_name?>">
+
+					<label for="feedback" class="education__label">
+						<?= esc_attr_x('And your email address?', 'feedback-form-question', 'svid-theme-domain')?>
+					</label>
+					<input type="email" name="email" class="education__input-short"
+						placeholder="<?= esc_attr_x('jamie@doe.com', 'feedback-form-placeholder', 'svid-theme-domain') ?>"
+						style="background-color: <?php theme_color(false); ?>"
+						value="<?=$current_email?>">
+
+				</div>
+
+
+				<div class="education__validate-and-send">
+
+					<input type="hidden" name="action" value="education_input">
+
+					<div class="g-recaptcha"
+						data-sitekey="6Ld7pCUUAAAAAFY2ezdhFaWW25L_c254ali_Hpsg">
+					</div>
+
+					<button type="submit" class="button education__submit">
+						<?= esc_attr_x('Send input', 'feedback-form-button', 'svid-theme-domain')?>
+					</button>
+
+				</div>
+
+		</form>
+	</section>
 
 
   <section class="education-about">

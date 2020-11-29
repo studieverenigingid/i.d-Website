4296ffd0ceb883793862dd7efb76d0db5f358564
Floris Jansen <florismartijnjansen@gmail.com>
2020-11-29 18:16:12 +0100

    preload & swap Muli

--- header.php
+++ header.php
@@ -18,6 +18,17 @@ global $img_folder;
 		<link rel="manifest" href="/manifest.json">
 		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f6b632">
 
+		<link rel="preload" href="/fonts/mulish-v1-latin-300.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-300.woff" as="font" type="font/woff" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-300.ttf" as="font" type="font/ttf" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-300.svg#Lora" as="font" type="font/svg" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-300.eot" as="font" type="font/eot" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-800.eot.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-800.eot.woff" as="font" type="font/woff" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-800.eot.ttf" as="font" type="font/ttf" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-800.eot.svg#Lora" as="font" type="font/svg" crossorigin>
+    <link rel="preload" href="/fonts/mulish-v1-latin-800.eot.eot" as="font" type="font/eot" crossorigin>
+
 		<?php $theme_info = wp_get_theme(); ?>
 
 		<?php wp_enqueue_style('main',

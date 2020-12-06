f0ddadcdc42cd6bc3ddead7a94727bea5268b7e0
Floris Jansen <florismartijnjansen@gmail.com>
2020-12-06 17:28:32 +0100

    standardize mail sending and form validation

--- inc/send-education.php
+++ inc/send-education.php
@@ -4,6 +4,7 @@
 
   /*
     Inputs possible (* if required):
+    - *subject
 		- *input
     - name
     - email
@@ -11,81 +12,31 @@
 
 	/**
 	 * Send the email and return the success rate
-	 *
-	 * @param string $input, string $name, string $email
 	 * @return bool
 	 */
-  function send_mail($input, $name, $email, $user_subject) {
+  function send_edu() {
+
+    $input = $_POST['feedback'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $user_subject = $_POST['subject'];
 
 		$options = get_option('id_settings');
     $receiver = $options['id_education_email_addresses_field'];
+
 		$subject = "Education input: $user_subject";
 
-    $message = "<!DOCTYPE html>
-        <html>
-         <head>
-          <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-          <title>Education input: $subject</title>
-          <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        </head>";
-    $message .= "<body><i>Sent using the contact form at " . get_site_url() .
-      "</i><br><br>" . esc_html($input);
-    $message .= "</body>";
-    $message .= "</html>";
+    $body = "<i>Sent using the contact form at " . get_site_url() . "</i>";
+    $body .= "<br><br>" . esc_html($input);
 
-    $message = wordwrap($message, 70);
-
-		if($email !== '') {
+    if($email !== '') {
 			$sender = "$name <$email>";
 		} else {
 			$sender = "Anonymous <anonymous@svid.nl>";
 		}
 
-		$headers = "From: $sender\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-    return wp_mail($receiver, $subject, $message, $headers);
+    return send_mail($receiver, $subject, $body, $sender);
 
   }
 
-
-
-	if (isset($_POST['submit'])) {
-
-    if (empty($_POST['special'])) {
-
-      $user_subject = $_POST['subject'];
-			$input = $_POST['feedback'];
-	    $name = $_POST['name'];
-	    $email = $_POST['email'];
-
-      /* VALIDATE INPUT */
-      $all_valid = true; // If this value isn't changed to false, it will send. Else it will be reported to the user.
-
-      if(!empty($input)) {
-        $send_return = send_mail($input, $name, $email, $user_subject);
-        if($send_return) {
-          $response['success'] = true;
-        } else {
-          $response['success'] = false;
-          $response['timestamp'] = date(DATE_ATOM);
-          $response['error'] = 'Something went wrong on the server. Apologies for the inconvenience. If this keeps happening, please contact us at svid@tudelft.nl.';
-        }
-      } else {
-        $response['success'] = false;
-				$response['error'] = 'You have not entered any input, or something else went wrong. Please try again! If this keeps happening, please contact us at svid@tudelft.nl.';
-      }
-
-    } else {
-      $response['success'] = false;
-      $response['error'] = 'It would appear you are a robot. If this keeps happening, please contact us at svid@tudelft.nl.';
-    }
-
-		wp_send_json($response);
-
-  } else {
-
-		wp_send_json(array( 'error' => 'no-submit' ));
-
-	}
+  validate_form('send_edu');

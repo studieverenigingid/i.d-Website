c33da0771376aa9f3f5e039976fa955cda07d0e8
Floris Jansen <florismartijnjansen@gmail.com>
2020-12-06 17:28:32 +0100

    standardize mail sending and form validation

--- inc/helpers.php
+++ inc/helpers.php
@@ -30,3 +30,84 @@ function send_failure($error_message, $status_code) {
 
 	}
 }
+
+
+
+
+
+/**
+ * Send the email and return the success rate
+ *
+ * @param string $message
+ * @param string $name
+ * @param string $email
+ * @return bool
+ */
+function send_mail($receiver, $subject, $body, $sender) {
+
+	$message = "<!DOCTYPE html>
+			<html>
+			 <head>
+				<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+				<title>$subject</title>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+			</head><body>";
+	$message .= $body;
+	$message .= "</body>";
+	$message .= "</html>";
+
+	$message = wordwrap($message, 70);
+
+	$headers = "From: $sender\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+	return wp_mail($receiver, $subject, $message, $headers);
+
+}
+
+
+
+
+
+/**
+ * Validate the form
+ *
+ * @param string $message
+ * @param string $name
+ * @param string $email
+ * @return bool
+ */
+function validate_form($callback) {
+	if (isset($_POST['submit'])) {
+
+		if (empty($_POST['special'])) {
+
+			if(!empty($_POST['feedback'])) {
+
+				$send_return = call_user_func($callback);
+
+				if($send_return) {
+					$response['success'] = true;
+				} else {
+					$response['success'] = false;
+					$response['timestamp'] = date(DATE_ATOM);
+					$response['error'] = 'Something went wrong on the server. Apologies for the inconvenience. If this keeps happening, please contact us at svid@tudelft.nl.';
+				}
+			} else {
+				$response['success'] = false;
+				$response['error'] = 'You have not entered any input, or something else went wrong. Please try again! If this keeps happening, please contact us at svid@tudelft.nl.';
+			}
+		} else {
+			$response['success'] = false;
+			$response['error'] = 'It would appear you are a robot. If this keeps happening, please contact us at svid@tudelft.nl.';
+		}
+
+		wp_send_json($response);
+
+	} else {
+
+		wp_send_json(array( 'error' => 'no-submit' ));
+
+	}
+}
